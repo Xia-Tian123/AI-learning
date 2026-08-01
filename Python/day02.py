eaae0537eb702673631f31a71f09b3851d94ac04
Xia-Tian123 <3190928975@qq.com>
2026-08-01 20:45:25 +0800

    Add day02.py

---
+++ Python/day02.py
@@ -0,0 +1,127 @@
+#标识符 注释
+"""
+第一个字符必须以字母（a-z, A-Z）或下划线 _
+禁止使用保留关键字，如 if、for、class 等不能作为标识符。
+
+非法例子：
+2nd_place = "silver"    # 错误：以数字开头
+user-name = "Bob"       # 错误：包含连字符
+class = "Math"          # 错误：使用关键字
+$price = 9.99          # 错误：包含特殊字符
+for = "loop"           # 错误：使用关键字
+"""
+
+姓名="夏天"#中文也可以
+
+'''
+检验标识符是否符合规定
+def is_valid_identifier(name):
+    try:
+        exec(f"{name} = None")
+        return True
+    except:
+        return False
+
+print(is_valid_identifier("2var"))  # False
+print(is_valid_identifier("var2"))  # True
+'''
+
+
+"""
+#行与缩进
+print ("hello python!")
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+ print ("False")    # 缩进不一致，会导致运行错误
+"""
+ 
+'''
+#多行
+item_one = 1
+item_two = 2
+item_three = 3
+total = item_one + \
+        item_two + \
+        item_three
+print(total) # 输出为 6
+在 [], {}, 或 () 中的多行语句，不需要使用反斜杠 \
+total = ['item_one', 'item_two', 'item_three',
+        'item_four', 'item_five']
+
+
+
+ #字符串
+str='123456789'
+ 
+print(str)                 # 输出字符串
+print(str[0:-1])           # 输出第一个到倒数第二个的所有字符
+print(str[0])              # 输出字符串第一个字符
+print(str[2:5])            # 输出从第三个开始到第六个的字符（不包含）
+print(str[2:])             # 输出从第三个开始后的所有字符
+print(str[1:5:2])          # 输出从第二个开始到第五个且每隔一个的字符（步长为2）
+print(str * 2)             # 输出字符串两次
+print(str + '你好')         # 连接字符串
+ 
+print('------------------------------')
+ 
+print('hello\nrunoob')      # 使用反斜杠(\)+n转义特殊字符
+print(r'hello\nrunoob')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+
+
+#同一行多个语句
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+脚本和交互式执行命令并不一样
+
+
+#print 输出
+print 默认输出是换行的，如果要实现不换行需要在变量末尾加上 end=""
+#!/usr/bin/python3
+ 
+x="a"
+y="b"
+# 换行输出
+print( x )
+print( y )
+ 
+print('---------')
+# 不换行输出
+print( x, end=" " )
+print( y, end=" " )
+print()
+
+
+#import 与 from...import
+在 python 用 import 或者 from...import 来导入相应的模块。
+
+将整个模块(somemodule)导入，格式为： import somemodule
+
+从某个模块中导入某个函数,格式为： from somemodule import somefunction
+
+从某个模块中导入多个函数,格式为： from somemodule import firstfunc, secondfunc, thirdfunc
+
+将某个模块中的全部函数导入，格式为： from somemodule import *
+import sys
+print('================Python import mode==========================')
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为',sys.path)
+
+
+
+
+#数据类型转换
+- `int()`：将一个数值或字符串转换成整数，可以指定进制。
+- `float()`：将一个字符串转换成浮点数。
+- `str()`：将指定的对象转换成字符串形式，可以指定编码。
+- `chr()`：将整数转换成该编码对应的字符串（一个字符）。
+- `ord()`：将字符串（一个字符）转换成对应的编码（整数）
+'''
+
+
+
+
