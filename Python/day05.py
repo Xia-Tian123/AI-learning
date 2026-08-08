aec9a95e10f15b82b55d27c6ad3476694f66342f
Xia-Tian123 <3190928975@qq.com>
2026-08-08 21:01:34 +0800

    add python day05

---
+++ Python/day05.py
@@ -0,0 +1,184 @@
+'''
+#列表
+
+访问数值
+list = ['Google', 'Runoob', "Zhihu", "Taobao", "Wiki"]
+# 读取第二位
+print ("list[1]: ", list[1])
+# 从第二位开始（包含）截取到倒数第二位（不包含）
+print ("list[1:-2]: ", list[1:-2])
+
+更新列表
+list = ['Google', 'Runoob', 1997, 2000]
+list[2] = 2001
+list1.append('Baidu')
+
+删除列表
+del list[2]
+
+len([1, 2, 3])	3	长度
+[1, 2, 3] + [4, 5, 6]	[1, 2, 3, 4, 5, 6]	组合
+['Hi!'] * 4	['Hi!', 'Hi!', 'Hi!', 'Hi!']	重复
+3 in [1, 2, 3]	True	元素是否存在于列表中
+for x in [1, 2, 3]: print(x, end=" ")	1 2 3	迭代
+
+函数
+len(list)
+列表元素个数
+
+max(list)
+返回列表元素最大值
+
+min(list)
+返回列表元素最小值
+
+list(seq)
+将元组转换为列表
+
+list.append(obj)
+在列表末尾添加新的对象
+
+list.count(obj)
+统计某个元素在列表中出现的次数
+
+list.extend(seq)
+在列表末尾一次性追加另一个序列中的多个值（用新列表扩展原来的列表）
+
+list.index(obj)
+从列表中找出某个值第一个匹配项的索引位置
+
+list.insert(index, obj)
+将对象插入列表
+
+list.pop([index=-1])
+移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
+
+list.remove(obj)
+移除列表中某个值的第一个匹配项
+
+list.reverse()
+反向列表中元素
+
+list.sort( key=None, reverse=False)
+对原列表进行排序
+
+list.clear()
+清空列表
+
+list.copy()
+复制列表
+'''
+
+'''
+#分支结构
+#### 例子1：英制单位英寸与公制单位厘米互换。
+#英制单位英寸和公制单位厘米互换
+value = float(input('请输入长度: '))
+unit = input('请输入单位: ')
+if unit == 'in' or unit == '英寸':
+    print('%f英寸 = %f厘米' % (value, value * 2.54))
+elif unit == 'cm' or unit == '厘米':
+    print('%f厘米 = %f英寸' % (value, value / 2.54))
+else:
+    print('请输入有效的单位')
+
+
+
+#百分制成绩转换为等级制成绩
+score = float(input('请输入成绩: '))
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'E'
+print('对应的等级是:', grade)
+
+#### 例子3：输入三条边长，如果能构成三角形就计算周长和面积。
+#判断输入的边长能否构成三角形，如果能则计算出三角形的周长和面积
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    peri = a + b + c
+    print(f'周长: {peri}')
+    half = peri / 2
+    area = (half * (half - a) * (half - b) * (half - c)) ** 0.5
+    print(f'面积: {area}')
+else:
+    print('不能构成三角形')
+
+
+
+# f用for循环实现1~100之间的偶数求和
+total = 0
+for x in range(2, 101, 2):
+    total += x
+print(total)
+
+
+# while循环
+#猜数字游戏
+import random
+# 产生一个1-100范围的随机数
+answer = random.randint(1, 100)
+counter = 0
+while True:
+    counter += 1
+    number = int(input('请输入: '))
+    if number < answer:
+        print('大一点')
+    elif number > answer:
+        print('小一点')
+    else:
+        print('恭喜你猜对了!')
+        break
+# 当退出while循环的时候显示用户一共猜了多少次
+print(f'你总共猜了{counter}次')
+
+
+#打印乘法口诀表
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f'{i}*{j}={i * j}', end='\t')
+    print()
+
+### 循环的例子
+
+#### 例子1：输入一个正整数判断它是不是素数。
+
+输入一个正整数判断它是不是素数
+
+num = int(input('请输入一个正整数: '))
+end = int(num ** 0.5)
+is_prime = True
+for x in range(2, end + 1):
+    if num % x == 0:
+        is_prime = False
+        break
+if is_prime and num != 1:
+    print(f'{num}是素数')
+else:
+    print(f'{num}不是素数')
+
+
+#### 例子2：输入两个正整数，计算它们的最大公约数和最小公倍数。
+
+x = int(input('x = '))
+y = int(input('y = '))
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print(f'{x}和{y}的最大公约数是{factor}')
+        print(f'{x}和{y}的最小公倍数是{x * y // factor}')
+        break
+
+'''
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(f'{i}*{j}={i*j}',end='\t')
+    print()
