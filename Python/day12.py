536a2c4cc48fbf3ba4c9114e9fcc271362943a96
Xia-Tian123 <3190928975@qq.com>
2026-08-18 22:31:50 +0800

    add python day12

--- Python/day12.py
+++ Python/day12.py
@@ -177,3 +177,19 @@ print(product)  # 输出：120
 
 > **提示2**：上面使用的`all`函数是Python内置函数，如果传入的序列中所有布尔值都是`True`，`all`函数就返回`True`，否则`all`函数就返回`False`。
 '''
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x**2, numbers))
+print(squared)  # 输出: [1, 4, 9, 16, 25]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # 输出：[2, 4, 6, 8]
+
+from functools import reduce
+ 
+numbers = [1, 2, 3, 4, 5]
+ 
+# 使用 reduce() 和 lambda 函数计算乘积
+product = reduce(lambda x, y: x * y, numbers)
+ 
+print(product)  # 输出：120
